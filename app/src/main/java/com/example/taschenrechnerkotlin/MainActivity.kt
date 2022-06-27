db8c73643f85f66e9a154f630c3b366753575183
marcoweinmann <marco.wm94@gmail.com>
2022-06-27 14:47:17 +0200

    2

--- app/src/main/java/com/example/taschenrechnerkotlin/MainActivity.kt
+++ app/src/main/java/com/example/taschenrechnerkotlin/MainActivity.kt
@@ -5,18 +5,37 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val eingabe1 = findViewById<EditText>(R.id.editTextNumber)
+        val eingabe2 = findViewById<EditText>(R.id.editTextNumber2)
+        val plusButton = findViewById<Button>(R.id.button_plus)
+        val minusButton = findViewById<Button>(R.id.button_minus)
+        val geteiltButton = findViewById<Button>(R.id.buttonTeilen)
+        val MultiButton = findViewById<Button>(R.id.buttonMulti)
+        val ergebniss = findViewById<TextView>(R.id.Ergebniss_View)
+
+        plusButton.setOnClickListener {
+           ergebniss.text = (eingabe1.text.toString().toDouble() + eingabe2.text.toString().toDouble()).toString()
+        }
+
+        minusButton.setOnClickListener {
+            ergebniss.text = (eingabe1.text.toString().toDouble() - eingabe2.text.toString().toDouble()).toString()
+        }
+
+        geteiltButton.setOnClickListener {
+            ergebniss.text = (eingabe1.text.toString().toDouble() / eingabe2.text.toString().toDouble()).toString()
+        }
+
+        MultiButton.setOnClickListener {
+            ergebniss.text = (eingabe1.text.toString().toDouble() * eingabe2.text.toString().toDouble()).toString()
+        }
+
     }
-    val eingabe1 = findViewById<EditText>(R.id.editTextNumber).text
-    val eingabe2 = findViewById<EditText>(R.id.editTextNumber2).text
-    val plusButton = findViewById<Button>(R.id.button_plus).
-
-}
-
-fun Plus(){
 
 }
